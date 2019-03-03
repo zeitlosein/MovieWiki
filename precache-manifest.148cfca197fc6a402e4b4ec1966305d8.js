812b28fc17c408e0535a4f555024d3b1701a7374
Nikolas Smith <nikolassmith21@gmail.com>
2019-03-03 17:56:11 -0500

    Updates

---
+++ precache-manifest.148cfca197fc6a402e4b4ec1966305d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea96f6159a2faf74fa53",
+    "url": "/MovieWiki/static/js/runtime~main.ea96f615.js"
+  },
+  {
+    "revision": "b166f131bc4659f861dc",
+    "url": "/MovieWiki/static/js/main.b166f131.chunk.js"
+  },
+  {
+    "revision": "8acc00ef3b66808ee6a4",
+    "url": "/MovieWiki/static/js/2.8acc00ef.chunk.js"
+  },
+  {
+    "revision": "b166f131bc4659f861dc",
+    "url": "/MovieWiki/static/css/main.14ab5208.chunk.css"
+  },
+  {
+    "revision": "38382aa4205dbc081d15857fd53f92fc",
+    "url": "/MovieWiki/index.html"
+  }
+];
